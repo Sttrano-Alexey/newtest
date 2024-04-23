48f0fe34a95b40b24567190bce4db560cdebfb1f
Sttrano-Alexey <80354673+Sttrano-Alexey@users.noreply.github.com>
2024-04-23 10:31:27 +0300

    newto ProductList.jsx

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -10,7 +10,7 @@ const ProductList = () => {
   const [showAll, setShowAll] = useState(false); // Состояние для отслеживания показа всех элементов
 
   useEffect(() => {
-    fetch("newtest-hw52wc0qk-sttrano-alexeys-projects.vercel.app/src/DATA/products.json")
+    fetch("newtest-hw52wc0qk-sttrano-alexeys-projects.vercel.app/DATA/products.json")
       .then((res) => res.json())
       .then((products) => setProducts(products))
       .catch((error) => {
